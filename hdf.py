b017e8f4306ff2a836da266f56965d8bcfc04d4b
zjli_QMjYO <zjli@justhealth.cn>
2023-10-26 14:14:19 +0800

    增加代码

--- hdf.py
+++ hdf.py
@@ -3,6 +3,10 @@ from bs4 import BeautifulSoup
 import pandas as pd
 import os
 
+
+# 爬取好大夫网页的爬虫代码
+
+
 # 发送HTTP请求
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
